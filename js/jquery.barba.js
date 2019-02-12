d3e8959f05ae647d2ed53a351542c08446a4e97a
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-12 19:38:49 +0100

    Proyecto. FakePath error

--- js/jquery.barba.js
+++ js/jquery.barba.js
@@ -5,4 +5,23 @@ $.fn.parpadea = function() {
             $(this).fadeOut(2000);
         });
     });
+
+    $.fn.centrar=function(){
+        var elemento=$(this);
+        cambiarCss();
+
+        $(window).on("resize", cambiarCss);
+
+        function cambiarCss(){
+            var altoVentana=$(window).height();
+            var anchoVentana=$(window).width();
+
+            $(elemento).css({
+                "position":"absolute",
+                "left": anchoVentana/2-200,
+                "top": altoVentana/2-200,
+            });
+        };
+
+    };
 };
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(function(){
 
     $('#img').on('click', desplegable);
     $('#titulo').on('click', setTitulo);
+    $('#imagen').on('click', setImagen);
 
 });
 
@@ -47,17 +48,65 @@ function setTitulo() {
             <input id="cancelar" type="submit" value="Cancelar">
         </p>
     </form>`)
-    .appendTo('.principal');
+    .appendTo('.principal')
+    .centrar();
 
     $('#confirmar').on('click', function (e) {
         e.preventDefault();
         if ($('#contenido').val()) {
-            $(`<${$('#tamaño :selected').val()}>${$('#contenido').val()}</${$('#tamaño:selected').val()}>`)
+            $(`
+                <${$('#tamaño :selected').val()}>
+                ${$('#contenido').val()}
+                </${$('#tamaño:selected').val()}>
+            `)
             .appendTo('.principal');
             $(this).parent().parent().remove();
         } else {
             alert('Rellene el campo de "Titulo" o pulse "Cancelar" para salir.');
         }
+    });
+
+    $('#cancelar').on('click', function (e) {
+        e.preventDefault();
+        $(this).parent().parent().remove();
+    });
+
+}
+
+function setImagen() {
+    $(`<form class="pre-form" name="titulo">
+        <p>
+            <label for="setImagen">Imagen: </label>
+            <input id="setImagen" type="file" accept="image/x-png,image/gif,image/jpeg"/>
+        </p>
+        <p>
+            <img id="actual" class="img-insitu" src="src/photos/eduardo.png">
+        </p>
+        <p>
+            <label for="contenido">Pié de imagen: </label>
+            <input id="contenido" type="text">
+        </p>
+        <p>
+            <input id="confirmar" type="submit" value="Confirmar">
+            <input id="cancelar" type="submit" value="Cancelar">
+        </p>
+    </form>`)
+    .appendTo('.principal')
+    .centrar();
+
+    $('#setImagen').on('change', function () {
+        $('#actual').attr('src', `${$('#setImagen').val()}`);
+    });
+
+    $('#confirmar').on('click', function (e) {
+        e.preventDefault();
+        var img = `<img src="src/photos/${$('#setImagen :selected').val()}">`;
+
+        if ($('#contenido').val()) {
+            img+=`<figcaption>${$('#contenido').val()}</figcaption>`;
+        }
+        $(img).appendTo('.principal');
+        $(this).parent().parent().remove();
     });
 
     $('#cancelar').on('click', function (e) {
