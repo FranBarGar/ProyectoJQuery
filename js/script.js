09d83b57896249de6ceea2885b18a348db1a1264
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-15 19:36:12 +0100

    Proyecto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(function(){
     $('#img').on('click', desplegable);
     $('#titulo').on('click', setTitulo);
     $('#imagen').on('click', setImagen);
+    $('#alertBoton').on('click', setAlert);
 
 });
 
@@ -24,9 +25,50 @@ function desplegable() {
     }
 }
 
-function botonAlert(){
-    var button = $(`<button>`);
-    $('.principal').append()
+function setAlert(){
+    $(`<form class="pre-form" name="alert">
+        <p>
+            <label for="setTitulo">Titulo del boton: </label>
+            <input id="setTitulo" type="text">
+        </p>
+        <p>
+            <label for="contenido">Contenido del alert: </label>
+            <input id="contenido" type="text">
+        </p>
+        <p>
+            <input id="confirmar" type="submit" value="Confirmar">
+            <input id="cancelar" type="submit" value="Cancelar">
+        </p>
+    </form>`)
+    .appendTo('.principal')
+    .centrar();
+
+    $('#confirmar').on('click', function (e) {
+        e.preventDefault();
+        if ($('#contenido').val()) {
+            $(`
+                <div class="draggable">
+                    <button data="${$('#contenido').val()}">
+                    ${$('#setTitulo').val()}
+                    </button>
+                </div>
+            `)
+            .on('click', (e)=>{
+                alert($(e.target).attr('data'));
+            })
+            .draggable()
+            .appendTo('.principal');
+            $(this).parent().parent().remove();
+        } else {
+            alert('Rellene el campo de "Titulo" o pulse "Cancelar" para salir.');
+        }
+    });
+
+    $('#cancelar').on('click', function (e) {
+        e.preventDefault();
+        $(this).parent().parent().remove();
+    });
+
 }
 
 function setTitulo() {
@@ -55,10 +97,13 @@ function setTitulo() {
         e.preventDefault();
         if ($('#contenido').val()) {
             $(`
-                <${$('#tamaño :selected').val()}>
-                ${$('#contenido').val()}
-                </${$('#tamaño:selected').val()}>
+                <div class="draggable">
+                    <${$('#tamaño :selected').val()}>
+                    ${$('#contenido').val()}
+                    </${$('#tamaño:selected').val()}>
+                </div>
             `)
+            .draggable()
             .appendTo('.principal');
             $(this).parent().parent().remove();
         } else {
@@ -94,18 +139,38 @@ function setImagen() {
     .appendTo('.principal')
     .centrar();
 
+    var src;
+
     $('#setImagen').on('change', function () {
-        $('#actual').attr('src', `${$('#setImagen').val()}`);
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#actual').attr('src', e.target.result);
+                src = e.target.result;
+            }
+
+            reader.readAsDataURL(this.files[0]);
+        }
     });
 
     $('#confirmar').on('click', function (e) {
         e.preventDefault();
-        var img = `<img src="src/photos/${$('#setImagen :selected').val()}">`;
+        var img = '<div class="draggable">';
+
+        if (src) {
+            img += `<img src="${src}">`
+        } else {
+            img += '<img src="src/photos/eduardo.png">';
+        }
+
 
         if ($('#contenido').val()) {
             img+=`<figcaption>${$('#contenido').val()}</figcaption>`;
         }
-        $(img).appendTo('.principal');
+        $(img+'</div>')
+        .draggable()
+        .appendTo('.principal');
         $(this).parent().parent().remove();
     });
 
